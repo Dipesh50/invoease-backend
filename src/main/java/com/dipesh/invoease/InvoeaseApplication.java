21e748f72a84ed7d85432d88b7aa10f85b626b8c
Dipesh Solanki <dipeshsolanki161080@gmail.com>
2026-06-25 13:27:10 +0530

    Initial Spring Boot project setup with MySQL

---
+++ src/main/java/com/dipesh/invoease/InvoeaseApplication.java
@@ -0,0 +1,13 @@
+package com.dipesh.invoease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoeaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipesh/invoease/InvoeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipesh.invoease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
